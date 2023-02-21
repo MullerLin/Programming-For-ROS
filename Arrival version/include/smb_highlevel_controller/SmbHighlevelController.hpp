96a773bf3f6445017db6049c01ad8d2d00791b50
MullerLin <625512197@qq.com>
2023-02-22 05:18:05 +0800

    different version

--- Arrival version/include/smb_highlevel_controller/SmbHighlevelController.hpp
+++ Arrival version/include/smb_highlevel_controller/SmbHighlevelController.hpp
@@ -28,13 +28,12 @@ private:
 	ros::Publisher vis_pub;
 	ros::Subscriber subcriber_;
 	ros::Subscriber Point_sub;
-	ros::Publisher smb_vel_pub;
+	ros::Publisher Vel_pub;
 	std::string name_topic;
 	std::int32_t size_queue_topic;
 	std::float_t linear_gain;
 	std::float_t angular_gain;
 	visualization_msgs::Marker marker;
-//	geometry_msgs::Twist smb_vel_msg;
 	void pointcloudCallback(const sensor_msgs::PointCloud2& msg);
 	void scanCallback(const sensor_msgs::LaserScan& msg);
 	void Marker_publish(const float pos[]);
